4634f9b82dd15d67e16e7877bc2cf3cf3dcd50f1
hans <hansmrtn@pm.me>
2023-12-14 20:07:19 -0800

    Merge: Dev -> Main. (#3)
    * fix: extra cycle burn, unwraps to proper error handling on ws msgs
    
    * feat: remove encoding feature
    
    * feat: add docs hyperlink
    
    * update: google certs
    
    * fix: extra cycle burn, unwraps to proper error handling on ws msgs
    
    * feat: remove encoding feature
    
    * feat: add docs hyperlink
    
    * update: google certs
    
    * Update README.md
    
    * feat: refactor oidc validator
    
    simplifies the oidc validator
    
    * fix: update certs (#4)
    
    * add: re add local exec mode
    
    * feat: add lazy static, local exec runner, update 0.19.1
    
    * update: generalize the decoder
    
    * feat: optimize the decoder
    
    * Update README.md
    
    * improve findability of related resources with docs (#8)
    
    * add: readme zkvm
    
    * Update README.md
    
    * add: methods readme
    
    * add: host readme
    
    * add: contracts readme
    
    * add: oidc readme
    
    * add: links to demos readme
    
    * Update README.md

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # demos
 
-This repository contains the source code for a RISC Zero demo applicaitons.
+This repository contains the source code for RISC Zero demo applications. The projects are designed for demonstration purposes only and should not be used in production.
 
-## Warning / Security Notice
+To learn more about RISC Zero visit our [website] and developer [docs]. 
+
+### Warning / Security Notice
 Demo applications are provided as-is and have NOT BEEN AUDITED and the following demos contain KNOWN security findings:
 * bonsai-pay
 
 This code is provided as-is as a benefit to the public good, however should not be used with out a comprehensive security analyis.
+
+[website]: https://risczero.com
+[docs]: https://dev.risczero.com
---
+++ bonsai-pay/zkvm/oidc-validator/src/certs.rs
@@ -0,0 +1,22 @@
+pub static GOOGLE_PUB_JWK: &str = r#"
+{
+  "keys": [
+    {
+      "kid": "e4adfb436b9e197e2e1106af2c842284e4986aff",
+      "alg": "RS256",
+      "use": "sig",
+      "e": "AQAB",
+      "kty": "RSA",
+      "n": "psply8S991RswM0JQJwv51fooFFvZUtYdL8avyKObshyzj7oJuJD8vkf5DKJJF1XOGi6Wv2D-U4b3htgrVXeOjAvaKTYtrQVUG_Txwjebdm2EvBJ4R6UaOULjavcSkb8VzW4l4AmP_yWoidkHq8n6vfHt9alDAONILi7jPDzRC7NvnHQ_x0hkRVh_OAmOJCpkgb0gx9-U8zSBSmowQmvw15AZ1I0buYZSSugY7jwNS2U716oujAiqtRkC7kg4gPouW_SxMleeo8PyRsHpYCfBME66m-P8Zr9Fh1Qgmqg4cWdy_6wUuNc1cbVY_7w1BpHZtZCNeQ56AHUgUFmo2LAQQ"
+    },
+    {
+      "kid": "0ad1fec78504f447bae65bcf5afaedb65eec9e81",
+      "n": "sm72oBH-R2Rqt4hkjp66tz5qCtq42TMnVgZg2Pdm_zs7_-EoFyNs9sD1MKsZAFaBPXBHDiWywyaHhLgwETLN9hlJIZPzGCEtV3mXJFSYG-8L6t3kyKi9X1lUTZzbmNpE0tf-eMW-3gs3VQSBJQOcQnuiANxbSXwS3PFmi173C_5fDSuC1RoYGT6X3JqLc3DWUmBGucuQjPaUF0w6LMqEIy0W_WYbW7HImwANT6dT52T72md0JWZuAKsRRnRr_bvaUX8_e3K8Pb1K_t3dD6WSLvtmEfUnGQgLynVl3aV5sRYC0Hy_IkRgoxl2fd8AaZT1X_rdPexYpx152Pl_CHJ79Q",
+      "use": "sig",
+      "e": "AQAB",
+      "kty": "RSA",
+      "alg": "RS256"
+    }
+  ]
+}
+"#;
