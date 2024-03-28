e628923f18cb777233e437fb6ef797726926d303
hans <hmrtn@pm.me>
2024-03-28 11:55:16 -0700

    feat: update prove request to use new api

--- bonsai-pay/ui/src/components/Prove.tsx
+++ bonsai-pay/ui/src/components/Prove.tsx
@@ -6,8 +6,7 @@ interface ProveProps {
   email: string | null;
 }
 
-const Prove: React.FC<ProveProps> = (props) => {
-  const { disabled, email } = props;
+const Prove: React.FC<ProveProps> = ({ disabled, email }) => {
   const [isLoading, setIsLoading] = useState(false);
 
   const { VITE_API_HOST } = import.meta.env;
@@ -15,48 +14,37 @@ const Prove: React.FC<ProveProps> = (props) => {
   const handleClick = useCallback(async () => {
     setIsLoading(true);
 
-    // Initialize a WebSocket connection
-    const socket = new WebSocket(VITE_API_HOST);
     const jwtCookie = document.cookie
       .split("; ")
       .find((row) => row.startsWith("jwt="));
     const jwt = jwtCookie?.split("=")[1];
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const openHandler = (_event: Event) => {
-      const message = JSON.stringify({ jwt });
-      socket.send(message);
-    };
-
-    const messageHandler = (event: MessageEvent) => {
-      document.cookie = `snark=${event.data}`;
+    if (!jwt) {
+      console.error("JWT not found");
       setIsLoading(false);
-    };
+      return;
+    }
 
-    const errorHandler = (event: Event) => {
-      console.error("WebSocket error: ", event);
-      setIsLoading(false); // Set loading state to false if an error occurs
-    };
+    try {
+      const response = await fetch(`${VITE_API_HOST}/auth`, {
+        method: "GET",
+        headers: {
+          'X-Auth-Token': jwt
+        },
+      });
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const closeHandler = (_event: CloseEvent) => {
-      setIsLoading(false); // Set loading state to false when the socket is closed
-    };
-
-    // Setup WebSocket event listeners
-    socket.addEventListener("open", openHandler);
-    socket.addEventListener("message", messageHandler);
-    socket.addEventListener("error", errorHandler);
-    socket.addEventListener("close", closeHandler);
-
-    // Clean up the event listeners when the component unmounts or if the WebSocket closes
-    return () => {
-      socket.removeEventListener("open", openHandler);
-      socket.removeEventListener("message", messageHandler);
-      socket.removeEventListener("error", errorHandler);
-      socket.removeEventListener("close", closeHandler);
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+      if (response.ok) {
+        const data = await response.body;
+        // Handle your response data here
+        console.log("Success:", data);
+      } else {
+        throw new Error('Response not OK');
+      }
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    } finally {
+      setIsLoading(false);
+    }
   }, []);
 
   return (
