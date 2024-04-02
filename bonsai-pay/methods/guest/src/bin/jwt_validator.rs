bbb8282d64eff37b9d164d9be0ddd4f76d7ce3a8
hans <hmrtn@pm.me>
2024-04-02 13:35:54 -0700

    chore: cleanup deps and rm commented

--- bonsai-pay/methods/guest/src/bin/jwt_validator.rs
+++ bonsai-pay/methods/guest/src/bin/jwt_validator.rs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// use std::io::Read;
-
 use alloy_primitives::{Address, FixedBytes};
 use alloy_sol_types::SolValue;
 use oidc_validator::IdentityProvider;
