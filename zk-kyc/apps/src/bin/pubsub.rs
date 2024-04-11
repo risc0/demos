5a810e4b0ab2150b34c10f69bb10a1b4dda6ad7f
hans <hmrtn@pm.me>
2024-04-11 15:28:39 -0700

    update: server for auth code handler

--- zk-kyc/apps/src/bin/pubsub.rs
+++ zk-kyc/apps/src/bin/pubsub.rs
@@ -11,20 +11,20 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
 use alloy_primitives::{FixedBytes, U256};
 use alloy_sol_types::{sol, SolInterface, SolValue};
 use anyhow::Context;
 use apps::{BonsaiProver, TxSender};
 use clap::Parser;
-use log::info;
+use log::{info, error};
 use methods::JWT_VALIDATOR_ELF;
+use serde::Deserialize;
 use tokio::sync::oneshot;
-use warp::Filter;
+use warp::{reject::Reject, reply::with_header, Filter, Rejection};
 
 sol! {
-    interface IBonsaiPay {
-        function claim(address payable to, bytes32 claim_id, bytes32 post_state_digest, bytes calldata seal);
+    interface IzkKYC {
+        function mint(address payable to, bytes32 claim_id, bytes32 post_state_digest, bytes calldata seal);
     }
 
     struct Input {
@@ -59,11 +59,49 @@ struct Args {
     contract: String,
 }
 
+#[derive(Debug, Deserialize)]
+struct AuthorizationCodeRequest {
+    code: String,
+}
+
+#[derive(Deserialize)]
+struct TokenResponse {
+    access_token: String,
+    token_type: String,
+    expires_in: u32,
+    scope: String,
+    refresh_token: String,
+    refresh_expires_in: u32,
+    id_token: String,
+}
+
+// Custom rejection types
+#[derive(Debug)]
+struct EmptyTokenRejection;
+impl Reject for EmptyTokenRejection {}
+
+#[derive(Debug)]
+struct ProverComputationFailedRejection;
+impl Reject for ProverComputationFailedRejection {}
+
+#[derive(Debug)]
+struct TokenEndpointRequestFailedRejection;
+impl Reject for TokenEndpointRequestFailedRejection {}
+
+#[derive(Debug)]
+struct TokenResponseReadFailedRejection;
+impl Reject for TokenResponseReadFailedRejection {}
+
+#[derive(Debug)]
+struct InvalidTokenResponseRejection;
+impl Reject for InvalidTokenResponseRejection {}
+
 const HEADER_XAUTH: &str = "X-Auth-Token";
 
-async fn handle_jwt_authentication(token: String) -> Result<(), warp::Rejection> {
+async fn jwt_authentication_handler(token: String) -> Result<impl warp::Reply, Rejection> {
+    info!("JWT received");
     if token.is_empty() {
-        return Err(warp::reject::reject());
+        return Err(warp::reject::custom(EmptyTokenRejection));
     }
 
     info!("Token received: {}", token);
@@ -77,9 +115,61 @@ async fn handle_jwt_authentication(token: String) -> Result<(), warp::Rejection>
     });
 
     match rx.await {
-        Ok(_result) => Ok(()),
-        Err(_) => Err(warp::reject::reject()),
+        Ok(_result) => Ok(warp::reply()),
+        Err(_) => Err(warp::reject::custom(ProverComputationFailedRejection)),
     }
+}
+
+async fn authorization_code_handler(
+    auth_request: AuthorizationCodeRequest,
+) -> Result<impl warp::Reply, Rejection> {
+    info!("Authorization code received");
+    let code = auth_request.code;
+    let client_id = std::env::var("CLIENT_ID").expect("CLIENT_ID not found in environment");
+    let client_secret =
+        std::env::var("CLIENT_SECRET").expect("CLIENT_SECRET not found in environment");
+    let redirect_uri =
+        std::env::var("REDIRECT_URI").expect("REDIRECT_URI not found in environment");
+
+    let params = [
+        ("client_id", client_id.as_str()),
+        ("client_secret", client_secret.as_str()),
+        ("code", code.as_str()),
+        ("redirect_uri", redirect_uri.as_str()),
+        ("grant_type", "authorization_code"),
+    ];
+
+    let client = reqwest::Client::new();
+    let res = client
+        .post("https://api.idmelabs.com/oauth/token")
+        .form(&params)
+        .send()
+        .await
+        .map_err(|e| {
+            error!("Token endpoint request failed: {:?}", e);
+            warp::reject::custom(TokenEndpointRequestFailedRejection)
+        }
+        )?;
+
+    let body = res
+        .text()
+        .await
+        .map_err(|_e| warp::reject::custom(TokenResponseReadFailedRejection))?;
+    info!("ID.me token endpoint response: {}", body);
+
+    let token_response: TokenResponse =
+        serde_json::from_str(&body).map_err(|_e| warp::reject::custom(InvalidTokenResponseRejection))?;
+
+    let id_token = token_response.id_token;
+    info!("ID TOKEN: {}", id_token);
+
+    let id_token_cookie = format!("id_token={}; Path=/; Max-Age=3600; ", id_token);
+
+    Ok(with_header(
+        "Authenticated successfully",
+        "Set-Cookie",
+        id_token_cookie,
+    ))
 }
 
 fn prove_and_send_transaction(
@@ -88,15 +178,13 @@ fn prove_and_send_transaction(
     tx: oneshot::Sender<(Vec<u8>, FixedBytes<32>, Vec<u8>)>,
 ) {
     let input = Input {
-        identity_provider: U256::ZERO, // Google as the identity provider
+        identity_provider: U256::ZERO, // ID.me as the identity provider
         jwt: token,
     };
 
     let (journal, post_state_digest, seal) =
         BonsaiProver::prove(JWT_VALIDATOR_ELF, &input.abi_encode())
-            .expect("failed to prove on bonsai");
-
-    let seal_clone = seal.clone();
+            .expect("Failed to prove on Bonsai");
 
     let tx_sender = TxSender::new(
         args.chain_id,
@@ -104,59 +192,60 @@ fn prove_and_send_transaction(
         &args.eth_wallet_private_key,
         &args.contract,
     )
-    .expect("failed to create tx sender");
+    .expect("Failed to create tx sender");
 
     let claims = ClaimsData::abi_decode(&journal, true)
-        .context("decoding journal data")
-        .expect("failed to decode");
+        .context("Decoding journal data")
+        .expect("Failed to decode");
 
     info!("Claim ID: {:?}", claims.claim_id);
     info!("Msg Sender: {:?}", claims.msg_sender);
 
-    let calldata = IBonsaiPay::IBonsaiPayCalls::claim(IBonsaiPay::claimCall {
+    let calldata = IzkKYC::IzkKYCCalls::mint(IzkKYC::mintCall {
         to: claims.msg_sender,
         claim_id: claims.claim_id,
         post_state_digest,
-        seal: seal_clone,
+        seal: seal.clone(),
     })
     .abi_encode();
 
     // Send the calldata to Ethereum.
-    let runtime = tokio::runtime::Runtime::new().expect("failed to start new tokio runtime");
+    let runtime = tokio::runtime::Runtime::new().expect("Failed to start new tokio runtime");
     runtime
         .block_on(tx_sender.send(calldata))
-        .expect("failed to send tx");
+        .expect("Failed to send tx");
 
     tx.send((journal, post_state_digest, seal))
-        .expect("failed to send over channel");
+        .expect("Failed to send over channel");
 }
 
-fn jwt_authentication_filter() -> impl Filter<Extract = ((),), Error = warp::Rejection> + Clone {
-    warp::any()
+fn jwt_authentication_filter() -> impl Filter<Extract = (impl warp::Reply,), Error = Rejection> + Clone {
+    warp::path("authenticate")
         .and(warp::header::<String>(HEADER_XAUTH))
-        .and_then(handle_jwt_authentication)
+        .and_then(jwt_authentication_handler)
 }
 
-fn auth_filter() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-    let cors = warp::cors()
-        .allow_any_origin()
-        .allow_methods(vec!["GET", "POST", "DELETE"])
-        .allow_headers(vec!["content-type", "x-auth-token"])
-        .max_age(3600);
-
-    warp::path("auth")
+fn authorization_code_filter() -> impl Filter<Extract = (impl warp::Reply,), Error = Rejection> + Clone {
+    warp::path("callback")
         .and(warp::get())
-        .and(warp::path::end())
-        .and(jwt_authentication_filter().untuple_one())
-        .map(|| warp::reply())
-        .with(cors)
+        .and(warp::query::<AuthorizationCodeRequest>())
+        .and_then(authorization_code_handler)
 }
 
 #[tokio::main]
 async fn main() {
     env_logger::init();
 
-    let api = auth_filter();
+    let cors = warp::cors()
+        .allow_any_origin()
+        .allow_methods(vec!["GET", "POST"])
+        .allow_headers(vec!["content-type", "x-auth-token"])
+        .max_age(3600);
+
+    let jwt_route = jwt_authentication_filter();
+    let callback_route = authorization_code_filter();
+
+    let api = jwt_route.or(callback_route).with(cors);
 
     warp::serve(api).run(([127, 0, 0, 1], 8080)).await;
 }
