bb24a9699196ff1a395dd9d33cfef1ac669ac871
Cohan Carpentier <cohan.carpentier@gmail.com>
2024-05-03 15:41:27 -0400

    chore: update code snippet

--- zk-socials/src/app/(demo)/_components/code-preview.tsx
+++ zk-socials/src/app/(demo)/_components/code-preview.tsx
@@ -4,44 +4,53 @@ import { cn } from "@risc0/ui/cn";
 import { Highlight, themes } from "prism-react-renderer";
 
 const codeBlock = `
-use alloy_primitives::{Address, FixedBytes};
-use alloy_sol_types::SolValue;
+// Copyright 2024 RISC Zero, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#![no_main]
+
 use oidc_validator::IdentityProvider;
 use risc0_zkvm::guest::env;
-use risc0_zkvm::sha::rust_crypto::{Digest as _, Sha256};
+use serde::Deserialize;
 use std::io::Read;
 
-alloy_sol_types::sol! {
-    struct ClaimsData {
-        address msg_sender;
-        bytes32 claim_id;
-    }
-    struct Input {
-        uint256 identity_provider;
-        string jwt;
-    }
+risc0_zkvm::guest::entry!(main);
+
+#[derive(Deserialize)]
+struct Input {
+  iss: IdentityProvider,
+  jwt: String,
 }
 
 fn main() {
-    let mut input_bytes = Vec::u8::new();
-    env::stdin().read_to_end(&mut input_bytes).unwrap();
+  // Read user input
+  let mut input_str: String = String::new();
+  env::stdin()
+    .read_to_string(&mut input_str)
+    .expect("could not read input string");
 
-    let input: Input = Input::abi_decode(&input_bytes, true).unwrap();
+  // Deserialize user input
+  let input: Input =
+    serde_json::from_str(&input_str.as_str()).expect("could not deserialize input");
 
-    let identity_provider: IdentityProvider = input.identity_provider.into();
-    let jwt: String = input.jwt;
+  // Validate the JWT
+  let (email, public_key): (String, String) = input
+    .iss
+    .validate(&input.jwt)
+    .expect("failed to validate and decode");
 
-    let (claim_id, msg_sender) = identity_provider.validate(&jwt).unwrap();
-    let msg_sender: Address = Address::parse_checksummed(msg_sender, None).unwrap();
-    let claim_id: FixedBytes32 =
-        FixedBytes::from_slice(Sha256::digest(claim_id.as_bytes()).as_slice());
-    let output = ClaimsData {
-        msg_sender,
-        claim_id,
-    };
-    let output = output.abi_encode();
-
-    env::commit_slice(&output);
+  // Commit the email and public key to the journal
+  env::commit(&(email, public_key));
 }
 `;
 
