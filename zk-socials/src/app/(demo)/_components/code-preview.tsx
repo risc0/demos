43dc523c91ba4ae077f0ea69aca2788ec348e6af
hans <hmrtn@pm.me>
2024-05-08 15:16:39 -0700

    Update code-preview.tsx

--- zk-socials/src/app/(demo)/_components/code-preview.tsx
+++ zk-socials/src/app/(demo)/_components/code-preview.tsx
@@ -4,12 +4,10 @@ import { cn } from "@risc0/ui/cn";
 import { Highlight, themes } from "prism-react-renderer";
 
 const codeBlock = `
-#![no_main]
-
 use oidc_validator::IdentityProvider;
 use risc0_zkvm::{
-  guest::env,
-  sha::rust_crypto::{Digest as _, Sha256},
+    guest::env,
+    sha::rust_crypto::{Digest as _, Sha256},
 };
 use serde::{Deserialize, Serialize};
 use std::io::Read;
@@ -18,50 +16,54 @@ risc0_zkvm::guest::entry!(main);
 
 #[derive(Deserialize)]
 struct Input {
-  iss: IdentityProvider,
-  jwks: String,
-  jwt: String,
+    iss: IdentityProvider,
+    jwks: String,
+    jwt: String,
 }
 
 #[derive(Serialize)]
 struct Output {
-  email: Vec<u8>,
-  public_key: String,
-  jwks: Vec<u8>,
+    email: String,
+    public_key: String,
+    expiration: String,
+    issued_at: String,
+    jwks: String,
 }
 
 fn main() {
-  // Read user input
-  let mut input_str: String = String::new();
-  env::stdin()
-    .read_to_string(&mut input_str)
-    .expect("could not read input string");
+    // Read user input
+    let mut input_str: String = String::new();
+    env::stdin()
+        .read_to_string(&mut input_str)
+        .expect("could not read input string");
 
-  // Deserialize user input
-  let input: Input = serde_json::from_str(&input_str).expect("could not deserialize input");
+    // Deserialize user input
+    let input: Input = serde_json::from_str(&input_str).expect("could not deserialize input");
 
-  // Validate the JWT
-  let (email, public_key, jwks): (String, String, String) = input
-    .iss
-    .validate(&input.jwt, &input.jwks)
-    .expect("failed to validate and decode");
+    // Validate the JWT
+    let (email, public_key, expiration, issued_at, jwks) = input
+        .iss
+        .validate(&input.jwt, &input.jwks)
+        .expect("failed to validate and decode");
 
-  // Hash the email and issuer jwks
-  let email = Sha256::digest(email.as_bytes()).to_vec();
-  let jwks = Sha256::digest(jwks.as_bytes()).to_vec();
+    // Hash the email and issuer jwks
+    let email = hex::encode(Sha256::digest(email.as_bytes()));
+    let jwks = hex::encode(Sha256::digest(jwks.as_bytes()));
 
-  // Commit the output to the public journal
-  env::commit(&Output {
-    email,
-    public_key,
-    jwks,
-  });
+    // Commit the output to the public journal
+    env::commit(&Output {
+        email,
+        public_key,
+        expiration,
+        issued_at,
+        jwks,
+    });
 }
 `;
 
 export function CodePreview() {
   return (
-    <Highlight theme={themes.vsDark} code={codeBlock} language="typescript">
+    <Highlight theme={themes.vsDark} code={codeBlock} language="rust">
       {({ className, tokens, getLineProps, getTokenProps }) => (
         <pre className={cn(className, "overflow-x-auto text-[10px]")}>
           {tokens.map((line, index) => (
@@ -69,7 +71,7 @@ export function CodePreview() {
               key={`row-${
                 // biome-ignore lint/suspicious/noArrayIndexKey: ignore
                 index
-              }`}
+                }`}
               {...getLineProps({ line })}
             >
               {line.map((token, index) => (
@@ -77,7 +79,7 @@ export function CodePreview() {
                   key={`token-${
                     // biome-ignore lint/suspicious/noArrayIndexKey: ignore
                     index
-                  }`}
+                    }`}
                   {...getTokenProps({ token })}
                 />
               ))}
