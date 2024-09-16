aa71eb591d4f58ede62b163811ced4e758e0afc0
Cohan Carpentier <cohan.carpentier@gmail.com>
2024-09-16 15:51:59 -0400

    fix: proper mono font

--- zk-socials/tailwind.config.ts
+++ zk-socials/tailwind.config.ts
@@ -2,7 +2,14 @@ import tailwindConfig from "@risc0/ui/config/tailwind.config.base";
 import deepmerge from "deepmerge";
 import type { Config } from "tailwindcss";
 
-const config = deepmerge(tailwindConfig, {}) satisfies Config;
+const config = deepmerge(tailwindConfig, {
+	theme: {
+		fontFamily: {
+			sans: ["var(--font-europa-sans)", "system-ui"],
+			mono: ["var(--font-jetbrains-mono)"],
+		},
+	},
+}) satisfies Config;
 
 config.content = [
 	"./node_modules/@risc0/ui/**/*.{ts,tsx}",
