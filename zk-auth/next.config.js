926dbd246f6b75c66102df94bda5096760b6b43a
Cohan Carpentier <cohan.carpentier@gmail.com>
2024-09-17 12:48:14 -0400

    fix: vite

--- zk-auth/next.config.js
+++ zk-auth/next.config.js
@@ -3,28 +3,6 @@ import "./src/env.js";
 import { nextConfigBase } from "@risc0/ui/config/next.config.base.js";
 import deepmerge from "deepmerge";
 
-const config = deepmerge(nextConfigBase, {
-	async headers() {
-		return [
-			{
-				source: "/api/:path*",
-				headers: [
-					{
-						key: "Access-Control-Allow-Origin",
-						value: "*", // Set your origin
-					},
-					{
-						key: "Access-Control-Allow-Methods",
-						value: "GET, POST, PUT, DELETE, OPTIONS",
-					},
-					{
-						key: "Access-Control-Allow-Headers",
-						value: "Content-Type, Authorization",
-					},
-				],
-			},
-		];
-	},
-});
+const config = deepmerge(nextConfigBase, {});
 
 export default config;
