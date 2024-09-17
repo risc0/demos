9b473632c3c0c8d538825b4ec911ce58e5de9340
Cohan Carpentier <cohan.carpentier@gmail.com>
2024-09-17 15:16:27 -0400

    fix: wip

--- zk-auth/next.config.js
+++ zk-auth/next.config.js
@@ -4,27 +4,28 @@ import { nextConfigBase } from "@risc0/ui/config/next.config.base.js";
 import deepmerge from "deepmerge";
 
 const config = deepmerge(nextConfigBase, {
-	async headers() {
-		return [
-			{
-				// matching all API routes
-				source: "/api/:path*",
-				headers: [
-					{ key: "Access-Control-Allow-Credentials", value: "true" },
-					{ key: "Access-Control-Allow-Origin", value: "*" },
-					{
-						key: "Access-Control-Allow-Methods",
-						value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-					},
-					{
-						key: "Access-Control-Allow-Headers",
-						value:
-							"X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-					},
-				],
-			},
-		];
-	},
+  // biome-ignore lint/suspicious/useAwait: ignore
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
 });
 
 export default config;
--- zk-auth/src/app/api/bonsai/snark-proving/route.ts
+++ zk-auth/src/app/api/bonsai/snark-proving/route.ts
@@ -1,17 +1,14 @@
-import { NextRequest, NextResponse } from "next/server";
+import { type NextRequest, NextResponse } from "next/server";
 import { bonsaiSnarkProving } from "~/app/_lib/bonsai-proving";
 
 export async function POST(request: NextRequest) {
-	const { uuid } = await request.json();
+  const { uuid } = await request.json();
 
-	try {
-		const snarkUuid = await bonsaiSnarkProving({ uuid });
+  try {
+    const snarkUuid = await bonsaiSnarkProving({ uuid });
 
-		return NextResponse.json({ uuid: snarkUuid });
-	} catch (error) {
-		return NextResponse.json(
-			{ error: "Internal Server Error" },
-			{ status: 500 },
-		);
-	}
+    return NextResponse.json({ uuid: snarkUuid });
+  } catch (error) {
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+  }
 }
