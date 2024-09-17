f1b8cf30d50ff15fe21fe97501dca717efe548bb
Cohan Carpentier <cohan.carpentier@gmail.com>
2024-09-17 12:37:21 -0400

    fix: last try4

--- zk-auth/next.config.js
+++ zk-auth/next.config.js
@@ -3,6 +3,28 @@ import "./src/env.js";
 import { nextConfigBase } from "@risc0/ui/config/next.config.base.js";
 import deepmerge from "deepmerge";
 
-const config = deepmerge(nextConfigBase, {});
+const config = deepmerge(nextConfigBase, {
+	async headers() {
+		return [
+			{
+				source: "/api/:path*",
+				headers: [
+					{
+						key: "Access-Control-Allow-Origin",
+						value: "*", // Set your origin
+					},
+					{
+						key: "Access-Control-Allow-Methods",
+						value: "GET, POST, PUT, DELETE, OPTIONS",
+					},
+					{
+						key: "Access-Control-Allow-Headers",
+						value: "Content-Type, Authorization",
+					},
+				],
+			},
+		];
+	},
+});
 
 export default config;
--- zk-auth/src/middleware.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-
-export function middleware() {
-	const response = NextResponse.next();
-	response.headers.set("Access-Control-Allow-Origin", "*");
-	return response;
-}
