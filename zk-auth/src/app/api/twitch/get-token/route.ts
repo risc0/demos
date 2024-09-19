a66d839f85f64e8026421e865d47dc41cf95fec7
Cohan Carpentier <cohan.carpentier@gmail.com>
2024-09-19 10:26:25 -0400

    fix: add twitch tokens7

--- zk-auth/src/app/api/twitch/get-token/route.ts
+++ zk-auth/src/app/api/twitch/get-token/route.ts
@@ -3,23 +3,28 @@ import env from "~/env";
 
 async function getTwitchJWT(code: string) {
   try {
+    const params = new URLSearchParams();
+    params.append("client_id", env.TWITCH_CLIENT_ID);
+    params.append("client_secret", env.TWITCH_CLIENT_SECRET);
+    params.append("code", code);
+    params.append("grant_type", "authorization_code");
+    params.append("redirect_uri", "http://localhost:3000");
+
+    console.log("params", params);
+
     const response = await fetch("https://id.twitch.tv/oauth2/token", {
       method: "POST",
       headers: {
         "Content-Type": "application/x-www-form-urlencoded",
       },
-      body: JSON.stringify({
-        client_id: env.TWITCH_CLIENT_ID,
-        client_secret: env.TWITCH_CLIENT_SECRET,
-        code,
-        grant_type: "authorization_code",
-        redirect_uri: "http://localhost:3000",
-      }),
+      body: params,
     });
 
     console.log("response", response);
 
     if (!response.ok) {
+      const errorText = await response.text();
+      console.error("Twitch API Error:", response.status, errorText);
       throw new Error(`HTTP error! status: ${response.status}`);
     }
 
@@ -45,8 +50,6 @@ export async function POST(request: Request) {
   try {
     const { code } = await request.json();
     const jwt = await getTwitchJWT(code);
-
-    console.log("jwt", jwt);
 
     return NextResponse.json({ jwt });
   } catch (error) {
