b94c53839f736b5c0c82ff9cc817dc4bd0d718db
Cohan Carpentier <cohan.carpentier@gmail.com>
2024-10-07 11:53:35 -0400

    fix: types

--- zk-socials/src/middleware.ts
+++ zk-socials/src/middleware.ts
@@ -9,11 +9,14 @@ export function middleware(request: NextRequest) {
     const authParts = basicAuth.split(' ')
     if (authParts.length === 2) {
       const auth = authParts[1]
-      const decodedAuth = atob(auth)
-      const [user, pwd] = decodedAuth.split(':')
+      
+      if (typeof auth === 'string') {
+        const decodedAuth = atob(auth)
+        const [user, pwd] = decodedAuth.split(':')
 
-      if (user === 'root' && pwd === env.PASSWORD) {
-        return NextResponse.next()
+        if (user === 'root' && pwd === env.PASSWORD) {
+          return NextResponse.next()
+        }
       }
     }
   }
