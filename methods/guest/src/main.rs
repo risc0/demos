861837031068407632d481fe4cf6bf7c1272295a
Austin Abell <austinabell8@gmail.com>
2024-10-09 12:58:35 -0400

    moved logic into zkvm (some hacky serialization)

--- methods/guest/src/main.rs
+++ methods/guest/src/main.rs
@@ -1,13 +1,38 @@
+use alloy_consensus::Header;
+use alloy_primitives::Bytes;
+use alloy_rlp::Decodable;
+use alloy_trie::Nibbles;
+use arrayvec::ArrayVec;
 use risc0_zkvm::guest::env;
 
 fn main() {
-    // TODO: Implement your guest code here
+    // Read the program inputs through a framed slice of bytes
+    let mut len: u32 = 0;
+    env::read_slice(core::slice::from_mut(&mut len));
+    let mut bytes = vec![0u8; len as usize];
+    env::read_slice(&mut bytes);
 
-    // read the input
-    let input: u32 = env::read();
+    let (encoded_header, encoded_index, proof_tx, proof_nodes): (
+        Vec<u8>,
+        ArrayVec<u8, 9>,
+        Vec<u8>,
+        Vec<Bytes>,
+    ) = postcard::from_bytes(&bytes).expect("failed to deserialize input");
 
-    // TODO: do something with the input
+    // Note: this is a bit of a hack to input rlp encoded bytes, but alloy header serialization is
+    // broken for non-self describing serialization protocols.
+    let header: Header = Header::decode(&mut &encoded_header[..]).unwrap();
 
-    // write public output to the journal
-    env::commit(&input);
+    let block_hash = header.hash_slow();
+
+    alloy_trie::proof::verify_proof(
+        header.transactions_root,
+        Nibbles::unpack(&encoded_index),
+        Some(proof_tx.clone()),
+        proof_nodes.iter(),
+    )
+    .unwrap();
+
+    // Commit that the encoded transaction is included in the block.
+    env::commit(&(block_hash, proof_tx));
 }
